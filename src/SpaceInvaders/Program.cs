283e6a1255b87c701e0532252f9956c06e07b91c
barracoder <barracoder@protonmail.ch>
2026-01-16 01:01:27 +0000

    feat: 12 - Game states

--- src/SpaceInvaders/Program.cs
+++ src/SpaceInvaders/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpaceInvaders;
+using SpaceInvaders.Entities;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<GameStateService>();
 
 await builder.Build().RunAsync();
